b2a4b96114fa3901bea9b810d5fc32bcd6083403
Vivek Singhal <48623606+viveksinghal1@users.noreply.github.com>
2022-10-02 16:41:10 +0530

    add class to navbar container

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 const Navbar = ({ title }) => {
     return (
         <nav className='navbar mb-12 shadow-lg bg-neutral text-neutrat-content'>
-            <div className="container mx-auto">
+            <div className="container mx-auto text-primary-content">
                 <div className="flex-none px-2 mx-2">
                     <FaGithub className='inline pr-2 text-3xl' />
                     <NavLink to='/' className='text-lg font-bold align-middle'>
